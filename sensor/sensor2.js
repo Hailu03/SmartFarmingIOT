e796afbb3dc2ea1708d610e2d908b5e16109bdb4
Hailu03 <10421016@student.vgu.edu.vn>
2024-06-12 20:39:13 +0700

    Update

--- sensor/sensor2.js
+++ sensor/sensor2.js
@@ -18,10 +18,6 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function getRandomFloat(min, max) {
-  return Math.random() * (max - min) + min;
-}
-
 async function sendSensorData(data) {
   console.log("Sending data:", data);
   try {
